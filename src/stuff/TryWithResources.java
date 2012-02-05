94249a3959082eb7990501d8e68a1fed84065330
Nikolay Botev <bono8106@yahoo.com>
2012-02-04 18:42:37 -0800

    try with resources sequence

---
+++ src/stuff/TryWithResources.java
@@ -0,0 +1,34 @@
+package stuff;
+
+public class TryWithResources {
+  public static void main(String[] args) throws Exception {
+    try (OpenDoor door = new OpenDoor();OpenWindow window = new OpenWindow()  ){
+               door.swing();
+               window.crank();
+    }
+    catch(Exception e) {System.out.println("Is there a draft?");}
+     finally {System.out.println("I'm putting a sweater on, regardless.");}
+  }
+}
+
+class OpenDoor implements AutoCloseable {
+  public OpenDoor() { System.out.println("The door is open."); }
+  public void swing() throws Exception {
+    System.out.println("The door is becoming unhinged.");
+    throw new Exception();
+  }
+  public void close() throws Exception {
+    System.out.println("The door is closed.");
+  }
+}
+
+class OpenWindow implements AutoCloseable {
+  public OpenWindow() { System.out.println("The window is open."); }
+  public void crank() throws Exception {
+    System.out.println("The window is overcranked!");
+    throw new Exception();
+  }
+  public void close() throws Exception{
+    System.out.println("The window is closed.");
+  }
+}
